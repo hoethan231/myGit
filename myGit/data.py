0cebf7c0b5dfb3cc83d52d4ca412bee924e4bcc1
hoethan231 <Icydragons1357@gmail.com>
2024-06-03 20:14:22 -0700

    implement object types

--- myGit/data.py
+++ myGit/data.py
@@ -7,12 +7,22 @@ def init():
     os.makedirs(GIT_DIR)
     os.makedirs(f'{GIT_DIR}/objects')
     
-def hash_object(data):
-    objectID = hashlib.sha1(data).hexdigest()
+def hash_object(data, type_="blob"):
+    
+    object = type_.encode() + b'\x00' + data
+    objectID = hashlib.sha1(object).hexdigest()
+    
     with open(f'{GIT_DIR}/objects/{objectID}', "wb") as out:
-        out.write(data)
+        out.write(object)
     return objectID
 
-def get_object(objectID):
+def get_object(objectID, expected="blob"):
+    
     with open(f'{GIT_DIR}/objects/{objectID}', "rb") as f:
-        return f.read()
+        object = f.read()
+        
+    type_, nullByte, data = object.partition(b'\x00')
+    
+    if expected is not None:
+        assert type_ == expected, f'Expected {expected}, got {type_}'
+    return data
